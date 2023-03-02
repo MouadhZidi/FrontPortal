b752bcffe6ab663721a23787490add30a0e38968
Mouadh Zidi <mouadhzidi@gmail.com>
2023-03-02 12:22:02 +0100

    historique

---
+++ src/app/pages/list-notifications/historique-notifications.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+const httpOptions = {
+  headers: new HttpHeaders({ "Content-Type": "application/json" }),
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HistoriqueNotificationService {
+
+
+  private header: HttpHeaders;
+  constructor(private httpClient: HttpClient) {
+    this.header = new HttpHeaders();
+  }
+
+
+  GetHistorique = (matPers: any,codSoc: any): Observable<any[]> => {
+    return this.httpClient.get<any[]>(
+      "http://localhost:8082/notification/gethistoriquenotification/" + matPers + "/" + codSoc
+    );
+  };
+
+
+}
--- src/app/pages/list-notifications/list-notifications/list-notifications.component.ts
+++ src/app/pages/list-notifications/list-notifications/list-notifications.component.ts
@@ -5,6 +5,7 @@ import { WebsocketService } from "src/app/layouts/topbar/shared/services/websock
 import { PersonnelService } from "../../Employe/personnel.service";
 import { Project } from "../../projects/project.model";
 import { projectData } from "../../projects/projectdata";
+import { HistoriqueNotificationService } from "../historique-notifications.service";
 
 @Component({
   selector: "app-list-notifications",
@@ -14,104 +15,54 @@ import { projectData } from "../../projects/projectdata";
 export class ListNotificationsComponent implements OnInit {
   projectData: Project[];
   breadCrumbItems: Array<{}>;
-  list22: any[] = [];
+  ListHistorique: any[] = [];
   notifForm: FormGroup;
-  n: any;
-  lib: any;
-  listAllNotif: any;
-  pers: any = {
-    cod_soc: this.token.getUser().cod_soc,
-    mat_pers: this.token.getUser().matpers,
-  };
+ 
 
   constructor(
     private fb: FormBuilder,
-    private websocketService: WebsocketService,
     public token: TokenStorage,
-    private serv: PersonnelService
+    private serv: HistoriqueNotificationService
   ) {}
 
   ngOnInit(): void {
+    this.getNotification()
+
     this.breadCrumbItems = [
       { label: "Notifications" },
       { label: "Liste des notifications", active: true },
     ];
 
-    this.projectData = projectData;
-    this.getAllnotification();
-    this.getNotification();
+
+
     this.notifForm = this.fb.group({
       id_notif: [""],
       etat_notif: [""],
       type_notif: [""],
     });
+
   }
 
-  openModal(user) {
-    this.notifForm.patchValue({
-      id_notif: user.id_notif,
+  
+ 
 
-      etat_notif: user.etat_notif,
-      type_notif: user.type_notif,
-    });
-    this.lib = this.notifForm.get("type_notif").value;
-    console.log(this.lib);
+  getNotification(){
 
-    this.websocketService
-      .UpdateEtatNotif(this.notifForm.value)
-
-      .subscribe({
-        next: (res) => {
-          if (res) {
-            if (res) {
-              this.getAllnotification();
-              // this.toastr.success(' agence updated!', 'update effectuée avec succés.');
-              //  this.userForm.reset();
-            } else {
-              // this.toastr.error('Echec update', 'Problème de suppression.');
-            }
-          }
-        },
-      });
-  }
-
-  getNotification() {
-    if (this.token.getUser().role_portail == "RH") {
-      this.websocketService.GetNotifRh().subscribe(
-        (data) => {
-          this.list22 = data;
-        },
-        (err) => {
-          console.log(err);
-        }
+    this.serv.GetHistorique(this.token.getUser().matpers,this.token.getUser().cod_soc).subscribe(
+      data => {
+        this.ListHistorique=data
+        console.log("historiquenotif"+this.ListHistorique);
+        console.log("matricule"+this.token.getUser().matpers)
+        console.log("code societe"+this.token.getUser().cod_soc)
+       
+  
+      },
+      err => {
+        console.log(err);
+      }
       );
-    } else {
-      this.serv.getpersonnel(this.pers).subscribe(
-        (data) => {
-          this.n = this.pers.cod_serv;
-          this.websocketService
-            .GetNotifByMat(this.token.getUser().matpers)
-            .subscribe(
-              (data) => {
-                this.list22 = data;
-              },
-              (err) => {
-                console.log(err);
-              }
-            );
-        },
-        (err) => {
-          console.log(err);
-        }
-      );
-    }
-    console.log("serv" + this.n);
-  }
+  } 
 
-  getAllnotification() {
-    this.websocketService.GetAllNotif().subscribe((data) => {
-      this.listAllNotif = data;
-      console.log(this.listAllNotif);
-    });
-  }
 }
+
+
